da61d24198611ad1442594b75f3bd512347e82df
onurcc <onurcancelik@gmail.com>
2022-08-25 13:31:57 +0300

    Update ContactModel.cs

--- AjaxPostContacts/Models/ContactModel.cs
+++ AjaxPostContacts/Models/ContactModel.cs
@@ -1,13 +1,24 @@
-﻿using System;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AjaxPostContacts.Models
 {
     public class ContactModel
     {
         public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime RecordDate { get; set; }
         public int Age { get; set; }
         public string City { get; set; }
 
+    }
+
+    public enum Cities
+    {
+        İstanbul,
+        Ankara,
+        İzmir,
+        Bursa,
+        Adana
     }
 }
