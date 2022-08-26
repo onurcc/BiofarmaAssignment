614d6d692ca501a7145b07a27dc88558a289e919
Onur Can Çelik <onurcnc@outlook.com>
2022-08-26 09:50:26 +0300

    Fixed json post

--- AjaxPostContacts/Controllers/ContactController.cs
+++ AjaxPostContacts/Controllers/ContactController.cs
@@ -19,20 +19,8 @@ namespace AjaxPostContacts.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        //[HttpPost]
-        //public ActionResult AddContact( string name, DateTime recorddate, int age, string city)
-        //{
-        //    var contact = new ContactModel();
-
-        //    contact.Name = name;
-        //    contact.RecordDate = recorddate;
-        //    contact.Age = age;
-        //    contact.City = city;
-        //    return new JsonResult(new { data = contact });
-        //}
-
         [HttpPost]
-        public ContactModel AddContact(string name, DateTime recorddate, int age, string city)
+        public ActionResult AddContact(string name, DateTime recorddate, int age, string city)
         {
             var contact = new ContactModel();
 
@@ -41,22 +29,7 @@ namespace AjaxPostContacts.Controllers
             contact.Age = age;
             contact.City = city;
 
-            return contact;
+            return new JsonResult(new { data = contact });
         }
-
-        //[HttpPost]
-        //public ActionResult AddContact([FromBody] string result)
-        //{
-        //    JObject jsonResult = JObject.Parse(result);
-
-        //    var contact = new ContactModel();
-
-        //    contact.Name = jsonResult["name"].ToString();
-        //    contact.RecordDate = DateTime.Parse(jsonResult["recordDate"].ToString());
-        //    contact.Age = Int32.Parse(jsonResult["age"].ToString());
-        //    contact.City = jsonResult["City"].ToString();
-        //    return new JsonResult(new { data = contact });
-        //}
-
     }
 }
